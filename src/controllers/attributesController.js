9495e8369cb6c11c2f0603bd8b89b0e5d2ed8078
Vitor Roberto Kogawa de Moraes <vitor.kogawa.roberto33@gmail.com>
2022-05-10 08:05:07 -0300

    updatings attributes controller

--- src/controllers/attributesController.js
+++ src/controllers/attributesController.js
@@ -33,6 +33,15 @@ class AttributesController {
 
     findByID = async (request, response) => {
         try {
+            const attribute = await Attributes.findById(request.params.id);
+
+            if (attribute) {
+                return response.status(200).json(attribute);
+            } else {
+                return response
+                    .status(400)
+                    .json({ message: "Attribute not found." });
+            }
         } catch (error) {
             return response.status(400).json({ error });
         }
@@ -40,6 +49,27 @@ class AttributesController {
 
     update = async (request, response) => {
         try {
+            //verificando se o atributo a ser modificado existe
+            const IsAttribute = await Attributes.findById(request.params.id);
+
+            if (IsAttribute) {
+                const newAttributeData = {
+                    name: request.body.name,
+                    type: request.body.type,
+                    max_value: request.body.max_value,
+                    default_value: request.body.default_value,
+                    player_attr: request.body.player_attr,
+                    icon: request.body.icon,
+                };
+
+                await Attributes.updateOne(newAttributeData, null, { overwrite: false });
+
+                return response.status(200).json({ message: "sucess." });
+            } else {
+                return response
+                    .status(400)
+                    .json({ message: "attribute not found." });
+            }
         } catch (error) {
             return response.status(400).json({ error });
         }
@@ -47,6 +77,20 @@ class AttributesController {
 
     remove = async (request, response) => {
         try {
+            //verificando se o atributo a ser removido existe.
+            const IsAtribute = await Attributes.find({
+                _id: request.params.id,
+            });
+
+            if (IsAtribute) {
+                await Attributes.deleteOne(IsAtribute);
+
+                return response.status(200).json({ message: "sucess." });
+            } else {
+                return response
+                    .status(400)
+                    .json({ message: "attribute not found" });
+            }
         } catch (error) {
             return response.status(400).json({ error });
         }
