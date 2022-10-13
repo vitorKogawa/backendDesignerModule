590d2c7ea23f0c4f9a768f6d7f3bae6ba67da115
Vitor Roberto Kogawa de Moraes <v245582@dac.unicamp.br>
2022-10-13 17:27:22 -0300

    configuração das rotas das conexões entre os nós

--- src/routes/nodeConnection.routes.js
+++ src/routes/nodeConnection.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import nodeConnectionController from "./../controllers/nodeConnectionController";
+import nodeConnectionController from '../controllers/nodeConnectionController';
 
 const nodeConnectionRoutes = new Router();
 
@@ -7,8 +7,8 @@ nodeConnectionRoutes.post("/create", nodeConnectionController.create);
 nodeConnectionRoutes.get("/", nodeConnectionController.findAll);
 nodeConnectionRoutes.get("/:id", nodeConnectionController.findByID);
 nodeConnectionRoutes.get(
-    "/current/:id",
-    nodeConnectionController.findCurrentByID
+  '/current/:id',
+  nodeConnectionController.findCurrentByID,
 );
 
 export { nodeConnectionRoutes };
