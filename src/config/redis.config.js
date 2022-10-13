895bb0a74bc2d8e11d75b797ba227de17df8bc9f
Vitor Roberto Kogawa de Moraes <v245582@dac.unicamp.br>
2022-10-13 17:31:28 -0300

    configuração do redis

--- src/config/redis.config.js
+++ src/config/redis.config.js
@@ -2,20 +2,20 @@ import { createClient } from "redis";
 import "./env.config";
 
 const publisher = createClient({
-    port: process.env.REDIS_PUBLISHER_PORT,
-    host: process.env.REDIS_PUBLISHER_HOST,
-    password: process.env.REDIS_PUBLISHER_PASSWORD,
+  port: process.env.REDIS_PUBLISHER_PORT,
+  host: process.env.REDIS_PUBLISHER_HOST,
+  password: process.env.REDIS_PUBLISHER_PASSWORD,
 });
 
 const subscriber = createClient({
-    port: process.env.REDIS_SUBSCRIBER_PORT,
-    host: process.env.REDIS_SUBSCRIBER_HOST,
-    password: process.env.REDIS_SUBSCRIBER_PASSWORD,
+  port: process.env.REDIS_SUBSCRIBER_PORT,
+  host: process.env.REDIS_SUBSCRIBER_HOST,
+  password: process.env.REDIS_SUBSCRIBER_PASSWORD,
 });
 
 const redisConfig = {
-    publisher,
-    subscriber,
+  publisher,
+  subscriber,
 };
 
 export { redisConfig };
