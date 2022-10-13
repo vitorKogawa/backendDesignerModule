28cfb89bc1aaac72b4a61e6a1801e8033c6a9f7a
Vitor Roberto Kogawa de Moraes <v245582@dac.unicamp.br>
2022-10-13 17:31:52 -0300

    controller do redis

--- src/controllers/redis.controller.js
+++ src/controllers/redis.controller.js
@@ -1,13 +1,13 @@
-import { redisConfig } from "./../config/redis.config";
+import { redisConfig } from '../config/redis.config';
 
 class RedisController {
-    sendMessage = (request, response) => {
-        redisConfig.publisher.publish(
-            "topic-analytics",
-            JSON.stringify(request.body)
-        );
-        response.send("Publishing an Event using Redis");
-    };
+  sendMessage = (request, response) => {
+    redisConfig.publisher.publish(
+      'topic-analytics',
+      JSON.stringify(request.body),
+    );
+    response.send('Publishing an Event using Redis');
+  };
 }
 
 export default new RedisController();
