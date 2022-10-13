6d18895b5d20374051f9516f5f6ed2a7162a67bc
Vitor Roberto Kogawa de Moraes <v245582@dac.unicamp.br>
2022-10-13 17:28:59 -0300

    model e controller do player config

--- src/models/playerConfig.js
+++ src/models/playerConfig.js
@@ -3,37 +3,37 @@
     “attributes”: [embeddedDocument]
     “player_model”: embeddedDocument
 */
-import { mongoose } from "./../database/connection";
+import { mongoose } from '../database/connection';
 
 const playerConfigSchema = mongoose.Schema({
-    // _id: {
-    //     type: String,
-    //     required: true
-    // },
-    insert_name: {
-        type: Boolean,
-        default: false
-    },
-    progress: {
-        type: Boolean
-    },
-    inventory_max: {
-        type: Number
-    },
-    inventory_equipped_slots: {
-        type: Number
-    },
-    attribute_start: {
-        type: String
-    },
-    dice: {
-        //roll_number
-        type: Number
-    },
-    points: {
-        //max_limit
-        type: Number
-    }
+  // _id: {
+  //     type: String,
+  //     required: true
+  // },
+  insert_name: {
+    type: Boolean,
+    default: false,
+  },
+  progress: {
+    type: Boolean,
+  },
+  inventory_max: {
+    type: Number,
+  },
+  inventory_equipped_slots: {
+    type: Number,
+  },
+  attribute_start: {
+    type: String,
+  },
+  dice: {
+    // roll_number
+    type: Number,
+  },
+  points: {
+    // max_limit
+    type: Number,
+  },
 });
 
 const playerConfig = mongoose.model("playerConfig", playerConfigSchema);
