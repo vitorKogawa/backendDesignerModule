d34f3ba524232b997ae1344f344eaa164ec92747
Vitor Roberto Kogawa de Moraes <v245582@dac.unicamp.br>
2022-10-13 17:30:58 -0300

    refatoração da conexão com banco

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,14 +1,12 @@
 import mongoose from "mongoose";
-import { databaseConnection } from "./../config/databaseConnection.config";
+import { databaseConnection } from '../config/databaseConnection.config';
 
 mongoose.Promise = global.Promise;
 
 mongoose
-    .connect(
-        databaseConnection.databaseURL,
-        databaseConnection.connectionOptions
-    )
-    .then(() => console.log("Connected."))
-    .catch((error) => console.log('String de conexão: ', process.env.DATABASE_URL));
+  .connect(databaseConnection.databaseURL, databaseConnection.connectionOptions)
+  .then(() => console.log("Connected."))
+  .catch((error) => console.log('String de conexão: ', process.env.DATABASE_URL),
+  );
 
 export { mongoose };
