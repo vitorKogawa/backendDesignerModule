d0df8de4ba5d2167f0ae40b174475b7d6eb212f1
Vitor Roberto Kogawa de Moraes <v245582@dac.unicamp.br>
2022-10-13 17:27:58 -0300

    rotas do game

--- src/routes/game.routes.js
+++ src/routes/game.routes.js
@@ -1,10 +1,14 @@
 import { Router } from "express";
-import { games_img_upload } from './../config/multer.config'
+import { games_img_upload } from '../config/multer.config';
 import gameController from "../controllers/gameController";
 
 const gameRouter = new Router();
 
-gameRouter.post("/create", games_img_upload.single("image"), gameController.create);
+gameRouter.post(
+  "/create",
+  games_img_upload.single("image"),
+  gameController.create
+);
 gameRouter.get("/", gameController.findAll);
 gameRouter.get("/:id", gameController.findByID);
 gameRouter.get("/userGames/:id", gameController.findUserGamesByID);
