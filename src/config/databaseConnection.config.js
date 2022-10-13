a928f0512c5e42fd6bc8907a630da8d41eecc823
Vitor Roberto Kogawa de Moraes <v245582@dac.unicamp.br>
2022-10-13 17:31:44 -0300

    configuração dos parametros de conexão com banco

--- src/config/databaseConnection.config.js
+++ src/config/databaseConnection.config.js
@@ -1,14 +1,14 @@
-import "../config/env.config";
+import './env.config';
 
 const connectionOptions = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
 };
 
 const databaseConnection = {
-    databaseURL: process.env.DATABASE_URL,
-    connectionOptions,
+  databaseURL: process.env.DATABASE_URL,
+  connectionOptions,
 };
 
 export { databaseConnection };
