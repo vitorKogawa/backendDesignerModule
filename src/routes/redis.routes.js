f57ea2dfb83d4ba0c29f6f25f838e1643f7d7904
Vitor Roberto Kogawa de Moraes <v245582@dac.unicamp.br>
2022-10-13 17:26:40 -0300

    configuração das rotas do redis

--- src/routes/redis.routes.js
+++ src/routes/redis.routes.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
-import { redisConfig } from "./../config/redis.config";
-import redisController from "./../controllers/redis.controller";
+import { redisConfig } from '../config/redis.config';
+import redisController from '../controllers/redis.controller';
 
 const redisRouter = Router();
 
 redisRouter.post("/message/send", redisController.sendMessage);
 
 redisConfig.subscriber.on("message", (channel, message) => {
-    console.log(`Received data: ${message}`);
+  console.log(`Received data: ${message}`);
 });
 
 redisConfig.subscriber.subscribe("topic-analytics");
