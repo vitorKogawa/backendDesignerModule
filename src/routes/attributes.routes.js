2c076c69e87fdd6130e6679a687c7302f7d76708
Vitor Roberto Kogawa de Moraes <v245582@dac.unicamp.br>
2022-10-13 17:28:14 -0300

    rotas para os atributos

--- src/routes/attributes.routes.js
+++ src/routes/attributes.routes.js
@@ -1,10 +1,15 @@
 import { Router } from "express";
-import { attributes_img_upload } from './../config/multer.config'
-import attributesController from "./../controllers/attributesController";
+import { attributes_img_upload } from '../config/multer.config';
+import attributesController from '../controllers/attributesController';
 
 const attributeRoute = Router();
 
-attributeRoute.post("/create", attributes_img_upload.single("icon"), attributesController.create);
+attributeRoute.post(
+  "/create",
+  attributes_img_upload.single("icon"),
+  attributesController.create
+);
+attributeRoute.get("/", attributesController.findAll);
 attributeRoute.get("/:id", attributesController.findByID);
 attributeRoute.put("/:id", attributesController.update);
 attributeRoute.delete("/:id", attributesController.remove);
