126ee1ea24de2d66fe8ed59111c68c68ae59741c
Vitor Roberto Kogawa de Moraes <v245582@dac.unicamp.br>
2022-10-13 17:30:29 -0300

    model e controller dos events

--- src/controllers/eventsController.js
+++ src/controllers/eventsController.js
@@ -1,5 +1,5 @@
-import { Router } from 'express'
-import { Events } from './../models/events'
+import { Router } from 'express';
+import { Events } from '../models/events';
 
 class EventsController {
     create = async (request, response) => {
@@ -12,10 +12,10 @@ class EventsController {
                 value,
                 target_type,
                 target_id,
-                modifier
-            } = request.body
+                modifier,
+            } = request.body;
 
-            const newEvent = {
+            const newEvent = await Events.create({
                 name,
                 source_type,
                 source_id,
@@ -23,38 +23,48 @@ class EventsController {
                 value,
                 target_type,
                 target_id,
-                modifier
-            }
+                modifier,
+            })
 
-            return response.status(200).json({ newEvent }) 
+            return response.status(200).json({ newEvent });
         } catch (error) {
-            return response.status(400).json({ message_error: error })
+            return response.status(400).json({ message_error: error });
+        }
+    };
+
+    findAll = async (request, response) => {
+        try {
+            const events = await Events.find()
+
+            return response.status(200).json({ events })
+        } catch (error) {
+            return response.status(400).json({ error })
         }
     }
 
     find = async (request, response) => {
         try {
-            //code ...
+            // code ...
         } catch (error) {
-            return response.status(400).json({ message_error: error })
+            return response.status(400).json({ message_error: error });
         }
-    }
+    };
 
     update = async (request, response) => {
         try {
-            //code ...
+            // code ...
         } catch (error) {
-            return response.status(400).json({ message_error: error })
+            return response.status(400).json({ message_error: error });
         }
-    }
+    };
 
     remove = async (request, response) => {
         try {
-            //code ...
+            // code ...
         } catch (error) {
-            return response.status(400).json({ message_error: error })
+            return response.status(400).json({ message_error: error });
         }
-    }
+    };
 }
 
-export default new EventsController()
+export default new EventsController();
--- src/models/events.js
+++ src/models/events.js
@@ -3,38 +3,38 @@
     “attributes”: [embeddedDocument]
     “player_model”: embeddedDocument
 */
-import { mongoose } from "./../database/connection";
+import { mongoose } from '../database/connection';
 
 const eventSchema = mongoose.Schema({
-    // _id: {
-    //     type: String,
-    //     required: true
-    // },
-    name: {
-        type: String,
-        required: true
-    },
-    source_type: {
-        type: String
-    },
-    source_id: {
-        type: String
-    },
-    operator: {
-        type: String
-    },
-    value: {
-        type: Number
-    },
-    target_type: {
-        type: String
-    },
-    target_id: {
-            type: String
-    },
-    modifier: {
-            type: Number
-    }
+  // _id: {
+  //     type: String,
+  //     required: true
+  // },
+  name: {
+    type: String,
+    required: true,
+  },
+  source_type: {
+    type: String,
+  },
+  source_id: {
+    type: String,
+  },
+  operator: {
+    type: String,
+  },
+  value: {
+    type: Number,
+  },
+  target_type: {
+    type: String,
+  },
+  target_id: {
+    type: String,
+  },
+  modifier: {
+    type: String,
+  },
 });
 
 const Events = mongoose.model("events", eventSchema);
