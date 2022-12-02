ab5f3dedd1c7c4bb45be0bfed64d314e2517cd0e
Vitor Roberto Kogawa de Moraes <v245582@dac.unicamp.br>
2022-12-01 23:56:00 -0300

    build to deploy in react

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,13 +1,13 @@
 import express, { Router } from "express";
-import { resolve } from 'path';
-import { gameRouter } from './game.routes';
-import { nodeConnectionRoutes } from './nodeConnection.routes';
-import { labelRoutes } from './label.routes';
-import { gameNodeRoutes } from './gameNode.routes';
-import { attributeRoute } from './attributes.routes';
-import { eventsRoute } from './events.routes';
-import { playerConfigRoute } from './playerConfig.routes';
-import { rewardsRoute } from './rewards.routes';
+import { resolve } from "path";
+import { gameRouter } from "./game.routes";
+import { nodeConnectionRoutes } from "./nodeConnection.routes";
+import { labelRoutes } from "./label.routes";
+import { gameNodeRoutes } from "./gameNode.routes";
+import { attributeRoute } from "./attributes.routes";
+import { eventsRoute } from "./events.routes";
+import { playerConfigRoute } from "./playerConfig.routes";
+import { rewardsRoute } from "./rewards.routes";
 
 const routes = Router();
 
@@ -21,17 +21,23 @@ routes.use("/playerConfig", playerConfigRoute);
 routes.use("/rewards", rewardsRoute);
 
 routes.use(
-    '/home/card/img/attributes/',
-    express.static(resolve(__dirname, '../assets/img/attributes/')),
+    "/home/card/img/attributes/",
+    express.static(resolve(__dirname, "../assets/img/attributes/"))
 );
 
 routes.use(
-    '/home/card/img/games/',
-    express.static(resolve(__dirname, '../assets/img/games/')),
+    "/home/card/img/games/",
+    express.static(resolve(__dirname, "../assets/img/games/"))
 );
 
+routes.post("/login", (request, response) =>
+    response.status(200).json({ message: "ok" })
+);
 
-routes.post("/login", (request, response) => response.status(200).json({ "message": "ok" }))
-
+const baseDir = resolve(__dirname, "..", "..", "build");
+routes.use(express.static(baseDir));
+routes.get("*", (request, response) =>
+    response.sendFile("index.html", { root: baseDir })
+);
 
 export { routes };
